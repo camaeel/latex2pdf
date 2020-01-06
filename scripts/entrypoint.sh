9d23fe7e26ead4c5f2fc41a1ca7b5ef188c017fc
Kamil <12999736+camaeel@users.noreply.github.com>
2020-01-06 22:59:19 +0100

    tests

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 latexmk -pdf "$FILE.tex" -outdir=/output
+
+exit $?
---
+++ scripts/run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+cat > "/input/test.tex" <<- EOM
+\documentclass{article}
+\begin{document}
+First document. This is a simple example, with no 
+extra parameters or packages included.
+\end{document}
+EOM
+
+
+export FILE=test 
+env
+/scripts/entrypoint.sh
+
+RESULT=$?
+
+if [ $RESULT -eq 0 ]; then
+  echo "success"
+else
+  exit $RESULT
+fi
+
+if [ ! -f "/output/$FILE.pdf" ]; then 
+    exit -1
+fi
+
+exit 0
